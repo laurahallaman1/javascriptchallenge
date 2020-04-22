63a1cf7436a1905687148f91c4d7fa6d81cf74b8
laurahallaman1 <56806860+laurahallaman1@users.noreply.github.com>
2020-04-22 01:24:09 -0400

    commit
    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// D3 to select HTML nodes, for each one it adds a table row with the table data 
+function appendTable(data) {
+    d3.select("tbody").html("");
+    data.forEach((selection) => {
+        var tableRow = d3.select("tbody").append("tr");
+        Object.values(selection).forEach((value) => {
+            var tableData = tableRow.append("td");
+            tableData.text(value);
+        });
+    })
+}
+// show table with all relevant data 
+appendTable(tableData);
+
+// on click event, selects the user-input "datetime" information and filters the table, displaying the filtered values
+function clickEvent() {
+    d3.event.preventDefault();
+    var date = d3.select("#datetime").property("value");
+    var filterDateTime = tableData;
+    if (date) {
+        filterDateTime = filterDateTime.filter((row) => row.datetime === date);
+    }
+    appendTable(filterDateTime);
+}
+// on click event (button) filters the data 
+d3.selectAll("#filter-btn").on("click", clickEvent);
